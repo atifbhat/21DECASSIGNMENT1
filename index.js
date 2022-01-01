2aa3b6dd655b72eebb2c9e6ce38ff004a9ccc268
atifbhat <94857007+atifbhat@users.noreply.github.com>
2022-01-01 22:45:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var number = prompt("Enter a number to check whether the number is greater than 10?");
+
+if (number > 10) {
+    alert("number is greater than 10");
+
+} else {
+    alert("number is not greater than 10");
+
+}
